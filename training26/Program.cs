2b7852f8c14b0841c88dec3a4512c34ef9919a6a
Sanjaikumaran <sanjaikumaran.palani@trumpf.com>
2026-07-16 11:27:19 +0530

    Implemented class to find first cooler day for the list of days

--- training26/Program.cs
+++ training26/Program.cs
@@ -1,7 +1,12 @@
-﻿namespace training26;
+﻿using Training26;
+
+namespace training26;
 
 class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      int[] temperatures = [30, 31, 29, 32, 28, 27, 30, 25];
+      WaitForCoolerDay waitForCoolerDay = new ();
+      int[] result = waitForCoolerDay.FirstCoolerDay (temperatures);
+      Console.WriteLine (string.Join (", ", result));
    }
 }
