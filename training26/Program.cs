419a16b0a54ec455eebdf09ec3d3c4c7c9817cbf
Sanjaikumaran <sanjaikumaran.palani@trumpf.com>
2026-07-16 11:43:45 +0530

    Added console input logic

--- training26/Program.cs
+++ training26/Program.cs
@@ -4,9 +4,19 @@ namespace training26;
 
 class Program {
    static void Main (string[] args) {
-      int[] temperatures = [30, 31, 29, 32, 28, 27, 30, 25];
+      string? inputString = null;
+      do {
+         Console.Write ("Enter the list of temperature as CSV (30, 31, 29): ");
+         inputString = Console.ReadLine ();
+      } while (string.IsNullOrEmpty (inputString));
+      string[] inputChars = inputString.Split (",");
+      int[] inputNumbers = new int[inputChars.Length];
+      for (int i = 0; i < inputChars.Length; i++) {
+         inputNumbers[i] = int.Parse (inputChars[i]);
+      }
+
       WaitForCoolerDay waitForCoolerDay = new ();
-      int[] result = waitForCoolerDay.FirstCoolerDay (temperatures);
+      int[] result = waitForCoolerDay.FirstCoolerDay (inputNumbers);
       Console.WriteLine (string.Join (", ", result));
    }
 }
