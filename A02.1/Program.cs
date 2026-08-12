6cc6d0baed4ba245fbd2e37043c5c40493255d50
Sanjaikumaran <sanjaikumaran.palani@trumpf.com>
2026-08-12 14:56:50 +0530

    Expand range to 1–127 and refactor input handling

--- A02.1/Program.cs
+++ A02.1/Program.cs
@@ -12,29 +12,22 @@ using static System.ConsoleColor;
 
 do {
    int guess = 0;
-   WriteLine ("Think of a number between 1 and 100.\n");
+   WriteLine ("Think of a number between 1 and 127.\n");
    for (int bit = 6; bit >= 0; bit--) {
       int currentNumber = guess | (1 << bit);
-      if (currentNumber > 100) continue;
       if (AskYesNo ($"Is your number greater than or equal to {currentNumber}?")) guess = currentNumber;
    }
-   PrintMsg ($"Your number is {guess}!\n", Green);
+   var (msg, color) = guess < 1 ? ("Those answers don't match a number between 1 and 127.", Red) : ($"Your number is {guess}!\n", Green);
+   PrintMsg (msg, color);
 } while (AskYesNo ("Do you want to play again?"));
 
 bool AskYesNo (string question) {
    while (true) {
       Write ($"{question} (Y/N): ");
-      switch (ReadKey (true).Key) {
-         case ConsoleKey.Y:
-            PrintMsg ("Y", Blue);
-            return true;
-         case ConsoleKey.N:
-            PrintMsg ("N", Magenta);
-            return false;
-         default:
-            PrintMsg ("Please enter Y or N.", Red);
-            break;
-      }
+      var result = ReadKey (true).Key switch { ConsoleKey.Y => true, ConsoleKey.N => false, _ => (bool?)null };
+      var (msg, color) = result.HasValue ? (result.Value ? ("Y", Blue) : ("N", Magenta)) : ("Invalid input", Red);
+      PrintMsg (msg, color);
+      if (result.HasValue) return result.Value;
    }
 }
 
