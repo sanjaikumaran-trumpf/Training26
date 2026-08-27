9e2e67b32d2ed35e621e9066c91242931f16fe6d
Sanjaikumaran <sanjaikumaran.palani@trumpf.com>
2026-08-27 12:37:18 +0530

    Removed unwanted comments

--- A04/Program.cs
+++ A04/Program.cs
@@ -9,9 +9,7 @@
 
 Dictionary<char, int> letterFrequency = [];
 foreach (char ch in File.ReadAllText ("./word_list.txt").ToLower ())
-   // Counting the letter frequency
    if (ch is >= 'a' and <= 'z')
       letterFrequency[ch] = letterFrequency.GetValueOrDefault (ch) + 1;
-// Printing the seven most frequency letters
 foreach (var item in letterFrequency.OrderByDescending (x => x.Value).ThenBy (x => x.Key).Take (7))
    Console.WriteLine ($"{item.Key,2} {item.Value}");
