2588773e690e1f1f606460bde8f6c5f4605e72cd
Sanjaikumaran <sanjaikumaran.palani@trumpf.com>
2026-09-17 18:45:30 +0530

    Changed constant name

--- A06/Program.cs
+++ A06/Program.cs
@@ -10,9 +10,9 @@ using static System.Console;
 
 Write ("Press U to print only unique solutions or any key to print all: ");
 bool printUniqueOnly = ReadKey ().Key == ConsoleKey.U;
-const int noOfQns = 8;
+const int BOARD_SIZE = 8;
 List<int[]> solns = [];
-int[] queensPos = new int[noOfQns];
+int[] queensPos = new int[BOARD_SIZE];
 Array.Fill (queensPos, -1);
 Solve (queensPos, 0, solns);
 OutputEncoding = System.Text.Encoding.UTF8;
@@ -24,33 +24,33 @@ for (int i = 0; i < solns.Count; i++) {
 
 // Prints chess board and queens in the given places
 void PrintBoard (int[] queens) {
-   for (int i = 0; i < noOfQns; i++) {
+   for (int i = 0; i < BOARD_SIZE; i++) {
       // Top / middle border
       Write (i == 0 ? "┌" : "├");
-      for (int j = 0; j < noOfQns; j++)
-         Write ("───" + (j == noOfQns - 1 ? (i == 0 ? "┐" : "┤") : (i == 0 ? "┬" : "┼")));
+      for (int j = 0; j < BOARD_SIZE; j++)
+         Write ("───" + (j == BOARD_SIZE - 1 ? (i == 0 ? "┐" : "┤") : (i == 0 ? "┬" : "┼")));
       WriteLine ();
       // Rows with and without queen
-      for (int j = 0; j < noOfQns; j++)
+      for (int j = 0; j < BOARD_SIZE; j++)
          Write (queens[i] == j ? "│ ♛ " : "│   ");
       WriteLine ("│");
    }
    // Bottom
    Write ("└");
-   for (int j = 0; j < noOfQns; j++)
-      Write ("───" + (j == noOfQns - 1 ? "┘" : "┴"));
+   for (int j = 0; j < BOARD_SIZE; j++)
+      Write ("───" + (j == BOARD_SIZE - 1 ? "┘" : "┴"));
 }
 
 // Try to place a queen in the current row
 // Recursively solve the remaining rows and backtrack if needed
 void Solve (int[] queens, int row, List<int[]> solutions) {
-   if (row == noOfQns) {
+   if (row == BOARD_SIZE) {
       var solution = (int[])queens.Clone ();
       if (!printUniqueOnly || IsUnique (solution))
          solutions.Add (solution);
       return;
    }
-   for (int column = 0; column < noOfQns; column++) {
+   for (int column = 0; column < BOARD_SIZE; column++) {
       if (IsSafe (queens, row, column)) {
          queens[row] = column;
          Solve (queens, row + 1, solutions);
@@ -78,9 +78,9 @@ bool IsUnique (int[] solution) {
    return true;
 
    int[] Rotate (int[] soln) {
-      int[] temp = new int[noOfQns];
-      for (int i = 0; i < noOfQns; i++)
-         temp[soln[i]] = noOfQns - i - 1;
+      int[] temp = new int[BOARD_SIZE];
+      for (int i = 0; i < BOARD_SIZE; i++)
+         temp[soln[i]] = BOARD_SIZE - i - 1;
       return temp;
    }
 
